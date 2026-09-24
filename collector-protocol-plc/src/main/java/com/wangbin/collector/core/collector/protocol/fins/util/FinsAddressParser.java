9312e9daed6fc85a297ad1b413267844ebbef379
wangbin <769460162@qq.com>
2026-09-25 05:41:47 +0800

    修改提示

--- collector-protocol-plc/src/main/java/com/wangbin/collector/core/collector/protocol/fins/util/FinsAddressParser.java
+++ collector-protocol-plc/src/main/java/com/wangbin/collector/core/collector/protocol/fins/util/FinsAddressParser.java
@@ -78,10 +78,6 @@ public final class FinsAddressParser {
         if ("STRING".equals(normalizedType) && bitOffset != null) {
             throw new IllegalArgumentException("FINS STRING does not support bit offset");
         }
-        if (bitOffset != null && !"BOOLEAN".equals(normalizedType)) {
-            throw new IllegalArgumentException("FINS bit address requires BOOLEAN dataType");
-        }
-
         FinsByteOrder byteOrder = FinsByteOrder.from(effectiveConfig.get("byteOrder"), FinsByteOrder.BIG_ENDIAN);
         FinsWordOrder wordOrder = FinsWordOrder.from(effectiveConfig.get("wordOrder"), FinsWordOrder.BIG_ENDIAN);
         String canonical = memoryArea.name() + ":" + wordAddress
